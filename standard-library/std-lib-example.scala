630a6c543bb164b5dd14eb28bcfc6ffe1e6f4721
Samuel Pilkey <33938665+Spilkey@users.noreply.github.com>
2018-12-09 21:16:15 -0500

    Create std-lib-example.scala

---
+++ standard-library/std-lib-example.scala
@@ -0,0 +1,7 @@
+//Array and List are data  Structures
+//(index) is a standard lib function for indexing Arrays, Lists
+val array = Array(1,2,3,4)
+val list  = List(1,2,3,4)
+println("First element:" + array(0))
+println("First element:" + list(0))
+// uses (index)
